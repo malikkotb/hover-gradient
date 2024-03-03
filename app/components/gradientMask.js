e5afe2664e8a06b63a38bcd4c2cb00409a1ce52d
Malik Kotb <malikkotb@icloud.com>
2024-03-03 20:15:28 +0100

    alternative apporach

--- app/components/gradientMask.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function gradientMask() {
-  return (
-    <div className=''>
-        
-    </div>
-  )
-}
--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
-"use client"
+"use client";
 import { useState, useEffect } from "react";
-import gradientMask from "./components/gradientMask";
 function generateRandomString(length) {
   return Array.from(
     { length },
@@ -9,24 +8,40 @@ function generateRandomString(length) {
 }
 
 export default function Home() {
-  
   const [string, setString] = useState(""); // Initialize with empty string
 
   useEffect(() => {
     const randomString = generateRandomString(10000); // Now this runs only on client side
     setString(randomString);
-  }, []); 
-
+  }, []);
 
   return (
     <main className="bg-black flex justify-center items-center w-full h-screen">
-      <div className="h-[70vh] w-[50vw] text-white text-sm rounded-lg overflow-hidden"
-      // style={{
-      //   maskImage: 'radial-gradient(circle at center, rgba(0,0,0,1) 30%, rgba(0,0,0,0) 70%)',
-      //   WebkitMaskImage: 'radial-gradient(circle at center, rgba(0,0,0,1) 30%, rgba(0,0,0,0) 70%)'
-      // }}
+      <div
+        className="h-[70vh] w-[50vw] text-sm rounded-lg overflow-hidden
+      "
       >
-        <p onMouseMove={() => setString(generateRandomString(10000))} className="relative top-[-5%] whitespace-pre-wrap break-words">{string}</p>
+        <p
+          onMouseMove={() => setString(generateRandomString(10000))}
+          className="relative text-white top-[-5%] whitespace-pre-wrap break-words"
+        >
+          {string}
+        </p>
+      </div>
+      <div
+        className="h-[70vh] w-[50vw] bg-white opacity-50 text-sm rounded-lg overflow-hidden
+      "
+        style={{
+          maskImage: "radial-gradient(circle at center, rgba(0,0,0,1) 30%, rgba(0,0,0,0) 70%)",
+          WebkitMaskImage: "radial-gradient(circle at center, rgba(0,0,0,1) 30%, rgba(0,0,0,0) 70%)",
+        }}
+      >
+        {/* <p
+          onMouseMove={() => setString(generateRandomString(10000))}
+          className="relative text-white top-[-5%] whitespace-pre-wrap break-words"
+        >
+          {string}
+        </p> */}
       </div>
     </main>
   );
